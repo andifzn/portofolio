fe31280ed0f7bc0af82f63714dfe2185bf347e68
andifzn <andifauzan586@gmail.com>
2025-06-06 09:50:19 +0800

    done

---
+++ gsap/script.js
@@ -0,0 +1,19 @@
+gsap.from('.hero-text', {
+    delay: 1,
+    duration: 2,
+    y: 100,
+    opacity: 0
+})
+
+gsap.from ('.hero-image', {
+    delay: 1,
+    duration: 2,
+    x: 100,
+    opacity: 0
+})
+
+gsap.from ('.navbar', {
+    duration: 1.5,
+    y: -100,
+    opacity: 0
+})
