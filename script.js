d377d238dae9ae376b8f4a743c12d9b0fc801234
andifzn <andifauzan586@gmail.com>
2026-08-07 10:38:22 +0800

    done

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const navbarMenu = document.querySelector(".navbar-menu");
 const hamburgerMenu = document.getElementById("hamburger-menu");
 
-const lenis = new Lenis();
+const isDesktop = window.innerWidth >= 1024;
 
-function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
+if (isDesktop) {
+  // 2. Inisialisasi Lenis KHUSUS DESKTOP saja
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  });
+
+  // 3. Hubungkan Lenis dengan GSAP ScrollTrigger
+  lenis.on('scroll', ScrollTrigger.update);
+
+  // 4. Pengganti requestAnimationFrame biasa (Pakai GSAP Ticker biar fps stabil)
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+  // 5. Matikan lag smoothing GSAP biar ga ada delay melompat
+  gsap.ticker.lagSmoothing(0);
 }
-requestAnimationFrame(raf);
-
 hamburgerMenu.addEventListener("click", () => {
     navbarMenu.classList.toggle("pop-up");
 });
